6e96ee6f1d8a52659607dbdd227ca7a3574a9f0b
bin549 <549288160@qq.com>
2024-08-05 12:04:11 +0800

    feat: add footstep sounds.

---
+++ Assets/Scripts/PlayerFootsteps.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    private AudioSource footstepSound;
+    [SerializeField] private AudioClip[] footstepClip;
+    private CharacterController characterController;
+    public float volume_Min, volume_Max;
+    private float accumulatedDistance;
+    [SerializeField] private float wallSpeedDistance;
+
+    private void Awake()
+    {
+        this.footstepSound = GetComponent<AudioSource>();
+        this.characterController = GetComponentInParent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        this.CheckToPlayFootstepSound();
+    }
+
+
+    private void CheckToPlayFootstepSound()
+    {
+        if (this.characterController.velocity.sqrMagnitude > 0)
+        {
+            this.accumulatedDistance += Time.deltaTime;
+            if (this.accumulatedDistance > wallSpeedDistance)
+            {
+
+                this.footstepSound.volume = Random.Range(volume_Min, volume_Max);
+                this.footstepSound.clip = footstepClip[Random.Range(0, footstepClip.Length)];
+                this.footstepSound.Play();
+                this.accumulatedDistance = 0f;
+            }
+
+        }
+	}	
+}
