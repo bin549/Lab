318ac5248faabfe99482f74a9c2a80e0703dfaee
bin549 <549288160@qq.com>
2024-11-28 17:38:31 +0800

    chore: outline system

--- Assets/03.Scripts/common/object/InteractableObject.cs
+++ Assets/03.Scripts/common/object/InteractableObject.cs
@@ -2,20 +2,45 @@ using System;
 using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class InteractableObject : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI originText, detectedText;
-
+    private LineRenderer lineRenderer;
+    [SerializeField] private Transform lineStartPoint;
+    [SerializeField] private Transform lineEndPoint;
     public TextMeshProUGUI DetectedText => detectedText;
+    [SerializeField] private Outline outline;
+    
+    private void Awake() {
+        this.lineRenderer = gameObject.GetComponent<LineRenderer>();
+    }
+
+    private void Start() {
+        if (this.outline) {
+            this.outline.enabled = false;
+        }
+        this.InitDashedLine();
+    }
+
+    private void InitDashedLine() {
+        this.lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        this.lineRenderer.startWidth = 0.007f;
+        this.lineRenderer.endWidth = 0.007f;
+        this.lineRenderer.positionCount = 2;
+        this.lineRenderer.SetPosition(0, lineStartPoint.position);
+        this.lineRenderer.SetPosition(1, lineEndPoint.position);
+    }
 
     public bool IsInteractable() {
         return this.detectedText.gameObject.activeSelf;
     }
-    
+
     public void OnHintToggle(bool isToggle) {
         this.originText.gameObject.SetActive(!isToggle);
         this.detectedText.gameObject.SetActive(isToggle);
+        this.outline.enabled = isToggle;
     }
-    
+
     private void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.CompareTag("Player")) {
             this.OnHintToggle(true);
